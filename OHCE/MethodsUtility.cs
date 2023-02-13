6fc1483ae4b567d0aabe4eb689a5346e8aaa1289
Thomas Chollet <t.chollet85180@outlook.fr>
2023-02-13 21:12:55 +0100

    Etape 2

--- OHCE/MethodsUtility.cs
+++ OHCE/MethodsUtility.cs
@@ -9,13 +9,13 @@ namespace OHCE
     public static class MethodsUtility
     {
 
+        public enum Languages { Francais, English, Unknown }
+
         public static string Palyndrome(String phrase)
         {
             if (phrase.Equals("")) return null;
 
-            char[] stringArray = phrase.ToCharArray();
-            Array.Reverse(stringArray);
-            string reversedString = new string(stringArray);
+            string reversedString = ReverseString(phrase);
 
             StringBuilder stringB = new StringBuilder();
             stringB.Append("\nBonjour !\n");
@@ -23,15 +23,58 @@ namespace OHCE
             if (phrase == reversedString && !phrase.Equals(""))
             {
                 stringB.Append(phrase);
-                stringB.Append("\nBien joué !\n");
+                stringB.Append("\nBien joué !");
             }
             else
             {
                 stringB.Append(reversedString);
             }
 
-            stringB.Append("Au revoir !");
+            stringB.Append("\nAu revoir !");
             return stringB.ToString();
+        }
+
+        public static string PalyndromeLang(String phrase, String langue)
+        {
+            StringBuilder stringB = new StringBuilder();
+            Languages lang = Language(langue);
+
+            if (lang == Languages.Unknown) return "Langage inconnu ! Retour à l'étape 1\n\n" + Palyndrome(phrase);
+            else if (lang == Languages.Francais) stringB.Append("\nBonjour !\n");
+            else stringB.Append("\nHello !\n");
+
+
+            if (phrase == ReverseString(phrase) && !phrase.Equals(""))
+            {
+                stringB.Append(phrase);
+                if (lang == Languages.Francais) stringB.Append("\nBien joué !");
+                else stringB.Append("\nWell done !");
+            }
+            else
+            {
+                stringB.Append(ReverseString(phrase));
+            }
+
+            if (lang == Languages.Francais) stringB.Append("\nAu revoir!\n");
+            else stringB.Append("\nGoodbye !\n");
+
+            return stringB.ToString() ;
+
+        }
+
+        public static string ReverseString(String phrase)
+        {
+            string reversedString;
+            char[] stringArray = phrase.ToCharArray();
+            Array.Reverse(stringArray);
+            return reversedString = new string(stringArray);
+        }
+
+        public static Languages Language(String lang)
+        {
+            if (lang.Equals("francais", StringComparison.InvariantCultureIgnoreCase) || lang.Equals("français", StringComparison.InvariantCultureIgnoreCase)) return Languages.Francais;
+            else if (lang.Equals("anglais", StringComparison.InvariantCultureIgnoreCase) || lang.Equals("english", StringComparison.InvariantCultureIgnoreCase)) return Languages.English;
+            else return Languages.Unknown;
         }
 
     }
--- OHCE/Program.cs
+++ OHCE/Program.cs
@@ -1,7 +1,13 @@
 ﻿//Etape 1
 using OHCE;
-
-Console.WriteLine("Entrez une phrase : ");
+/*
+Console.WriteLine("Etape 1 - entrez une phrase : ");
 string maPhrase = Console.ReadLine();
-Console.WriteLine(MethodsUtility.Palyndrome(maPhrase));
+Console.WriteLine(MethodsUtility.Palyndrome(maPhrase));*/
 
+//Etape 2
+Console.WriteLine("\nEtape 2 - entrez la langue : français / english");
+string lang = Console.ReadLine();
+Console.WriteLine("\nEntrez une phrase : ");
+string maPhrase = Console.ReadLine();
+Console.WriteLine(MethodsUtility.PalyndromeLang(maPhrase, lang));
