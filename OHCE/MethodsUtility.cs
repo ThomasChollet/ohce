dca1b52ab639140706b3176d0d5af76c24ac5c9a
Thomas Chollet <t.chollet85180@outlook.fr>
2023-02-13 20:42:21 +0100

    Etape 1

---
+++ OHCE/MethodsUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE
+{
+    public static class MethodsUtility
+    {
+
+        public static string Palyndrome(String phrase)
+        {
+            if (phrase.Equals("")) return null;
+
+            char[] stringArray = phrase.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedString = new string(stringArray);
+
+            StringBuilder stringB = new StringBuilder();
+            stringB.Append("\nBonjour !\n");
+
+            if (phrase == reversedString && !phrase.Equals(""))
+            {
+                stringB.Append(phrase);
+                stringB.Append("\nBien joué !\n");
+            }
+            else
+            {
+                stringB.Append(reversedString);
+            }
+
+            stringB.Append("Au revoir !");
+            return stringB.ToString();
+        }
+
+    }
+}
---
+++ OHCE/Program.cs
@@ -0,0 +1,7 @@
+﻿//Etape 1
+using OHCE;
+
+Console.WriteLine("Entrez une phrase : ");
+string maPhrase = Console.ReadLine();
+Console.WriteLine(MethodsUtility.Palyndrome(maPhrase));
+
